5f60d5ce6d844fadb5365cc944c14ec8dc00893c
David Clemens <dclemens@outlook.com>
2024-08-25 10:21:50 -0500

    Create quote.py

---
+++ week_2/quote.py
@@ -0,0 +1,21 @@
+"""
+Author: David Clemens
+Date: 2024-08-25
+File Name: quote.py
+Description: This file outputs a quote by Mark Twain.
+"""
+
+
+# Define a function named `print_quote`
+def print_quote():
+    # Store the quote in a variable named `quote`
+    quote = "‘It’s not the size of the dog in the fight, it’s the size of the fight in the dog.’"
+
+    # Store the author's name in a variable named `author`
+    author = "– Mark Twain"
+
+    # Print the quote followed by the author's name
+    print(f"{quote} {author}")
+
+# Call the function to display the quote and author
+print_quote()
